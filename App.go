0f7cbdec498fee7d045dcaaef4e1eb858e2a9ac6
mnuma <mh10hiroaki@gmail.com>
2016-04-11 16:36:14 +0900

    init

---
+++ App.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+	"reflect"
+)
+
+func main() {
+	z := "ズン"
+	d := "ドコ"
+	zd := []string{z, d}
+	zundoko := []string{z, z, z, z, d}
+	fmt.Println(zundoko)
+
+	rand.Seed(time.Now().UnixNano())
+	var zundokoArry []string
+
+	go func() {
+		for {
+			randZundoko := zd[rand.Intn(2)]
+			zundokoArry = append(zundokoArry, randZundoko)
+			if len(zundokoArry) >= 5 {
+
+				fmt.Println(zundokoArry)
+				if reflect.DeepEqual(zundoko, zundokoArry) {
+					fmt.Println(zundokoArry)
+					break
+				}
+
+				zundokoArry = []string{}
+				continue
+			}
+		}
+	}()
+	fmt.Println("キ・ヨ・シ!")
+}
+
